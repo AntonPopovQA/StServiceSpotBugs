892c55b48421135eb6180b932f1a2d1c0fe5fb9a
Anton <toni-popov93@mail.ru>
2022-06-15 20:38:15 +0300

    SpotBugsAndCheckStyle

---
+++ src/test/java/ru/netology/spotbugs/StServiceSpotBugs/StatisticsServiceTest.java
@@ -0,0 +1,29 @@
+package ru.netology.spotbugs.StServiceSpotBugs;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatisticsServiceTest {
+        @Test
+        void findMax() {
+            StatisticsService service = new StatisticsService();
+
+            long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+            long expected = 12;
+
+            long actual = service.findMax(incomesInBillions);
+
+            Assertions.assertEquals(expected, actual);
+        }
+        @Test
+        void FindMaxAllBranch() {
+            StatisticsService service = new StatisticsService();
+
+            long[] incomesInBillions = {12, 13, 14, 15, 16, 17, 18, 19, 25, 23, 20};
+            long expected = 25;
+
+            long actual = service.findMax(incomesInBillions);
+
+            Assertions.assertEquals(expected,actual);
+        }
+    }
